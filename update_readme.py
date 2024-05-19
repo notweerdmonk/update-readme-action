4d21ab5325e64ac36b180c5335304c9d319e6532
notweerdmonk <wrdmnk@gmail.com>
2024-05-19 20:47:01 +0530

    Fix new readme content

--- update_readme.py
+++ update_readme.py
@@ -9,7 +9,6 @@ g = Github(github_token)
 # Update README function
 def update_readme():
     # Check if README.md exists in the repository
-    print(os.path.abspath('.'))
     if 'README.md' not in os.listdir():
         print("README.md not found. Skipping update.")
         return
@@ -27,15 +26,17 @@ def update_readme():
         readme_content = readme_file.read()
 
     # Find index of "Featured Repositories" section
-    start_index = readme_content.find("Featured Repositories")
-    end_index = readme_content.find("Other Projects")
+    start_marker = "<!-- Featured Repositories Start -->"
+    end_marker = "<!-- Featured Repositories End -->"
+    start_index = readme_content.find(start_marker)
+    end_index = readme_content.find(end_marker)
 
     # Update repository list under "Featured Repositories" section
     if start_index != -1 and end_index != -1:
-        new_readme_content = readme_content[:start_index + len("Featured Repositories")] + "\n\n" + repo_list + "\n" + readme_content[end_index:]
+        new_readme_content = readme_content[:start_index + len(start_marker] + "\n\n" + repo_list + "\n" + readme_content[end_index:]
     else:
         # If "Featured Repositories" section is not found, update entire README content
-        new_readme_content = f"# Latest Repositories\n\n{repo_list}\n{readme_content}"
+        new_readme_content = f"{readme_content}\n{start_marker}\n### Featured Repositories\n\n{repo_list}\n{end_marker}"
 
     # Update README.md
     with open('README.md', 'w') as readme_file:
